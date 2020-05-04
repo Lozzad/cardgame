a1545b09c4cbb920e244af791300c133a1242288
Laurie Dibble <laurie.dibble1@gmail.com>
2020-05-04 14:33:12 +0100

    added the stuuff

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour {
+    public GameObject canvas;
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject dropzone;
+    private GameObject startParent;
+    private Vector2 startPosition;
+
+    private void Awake () {
+        canvas = GameObject.Find ("Main Canvas");
+    }
+
+    void Update () {
+        if (isDragging) {
+            transform.position = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent (canvas.transform, true);
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision) {
+        isOverDropZone = true;
+        dropzone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D (Collision2D collision) {
+        isOverDropZone = false;
+        dropzone = null;
+    }
+
+    public void StartDrag () {
+        startPosition = transform.position;
+        startParent = transform.parent.gameObject;
+        isDragging = true;
+    }
+
+    public void EndDrag () {
+        isDragging = false;
+        if (isOverDropZone) {
+            transform.SetParent (dropzone.transform, false);
+        } else {
+            transform.position = startPosition;
+            transform.SetParent (startParent.transform, false);
+        }
+    }
+}
---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour {
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+    List<GameObject> cards = new List<GameObject> ();
+
+    void Start () {
+        cards.Add (Card1);
+        cards.Add (Card2);
+    }
+
+    public void OnClick () {
+        for (var i = 0; i < 5; i++) {
+            GameObject playerCard = Instantiate (cards[Random.Range (0, cards.Count)], Vector3.zero, Quaternion.identity);
+            playerCard.transform.SetParent (PlayerArea.transform, false);
+
+            GameObject enemyCard = Instantiate (cards[Random.Range (0, cards.Count)], Vector3.zero, Quaternion.identity);
+            enemyCard.transform.SetParent (EnemyArea.transform, false);
+        }
+    }
+}
